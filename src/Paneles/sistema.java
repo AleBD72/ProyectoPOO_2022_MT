b2f306d2cbb89c4c43b103a944f6c40c43742db9
Camila Mier Cadena <camimiercadena@gmail.com>
2022-08-31 00:05:01 -0500

    login y pág. inicio form

---
+++ src/Paneles/sistema.java
@@ -0,0 +1,14 @@
+package Paneles;
+
+import javax.swing.*;
+
+public class sistema extends JFrame {
+    private JPanel principal;
+    private JButton salirPBT;
+    private JLabel codUser;
+    private JLabel nomUser;
+    private JLabel mailUser;
+    private JLabel celUser;
+    private JLabel rolUser;
+    private JMenuBar menBar;
+}
